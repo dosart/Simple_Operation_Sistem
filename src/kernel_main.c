3701b61c8ebfadf725aa866623a4d014d118f3f9
dosart <Dysprosium92@ya.ru>
2023-04-06 18:07:15 +0300

    build: output with printf

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,20 +1,15 @@
 #include "descriptor_tables/gdt.h"
 #include "descriptor_tables/idt.h"
-#include "lib/string.h"
-
-char* hello = "Hello world!";
-int screen_size = 80 * 25;
+#include "lib/stdio.h"
 
 void kernel_main(void* multiboot, void* kstack)
 {
-    char* video = (char*)0xB8000;
-    char buff[screen_size + 1];
-    video[screen_size] = '\0';
-
-    memset(buff, ' ', screen_size);
-    strext(video, buff, 0x7);
-    strext(video, hello, 0x7);
+    clear();
+    printf("My first kernel!\n");
 
     gdt_init();
+    printf("Global Descriptor Table (GDT) init!\n");
+
     idt_init();
+    printf("Interrup Descriptor Table (IDT) init!\n");
 }
